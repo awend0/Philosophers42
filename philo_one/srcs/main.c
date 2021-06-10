b36d85c8a26a3ae59411856d7819e118f9cfeb73
hasv <mraymun@student.21-school.ru>
2021-06-10 16:52:50 +0300

    delete philo_two :((

--- philo_one/srcs/main.c
+++ philo_one/srcs/main.c
@@ -1,6 +1,6 @@
 #include "../includes/philosophers.h"
 
-static void	free_all(t_philo *philos)
+static int	free_all(t_philo *philos)
 {
 	int		i;
 
@@ -8,15 +8,20 @@ static void	free_all(t_philo *philos)
 	i = 0;
 	while (i < philos->params->amount)
 	{
+		pthread_mutex_unlock(&philos->params->m_forks[i]);
 		pthread_mutex_destroy(&philos->params->m_forks[i]);
+		pthread_mutex_unlock(&philos[i].m_eat);
 		pthread_mutex_destroy(&philos[i].m_eat);
 		i++;
 	}
+	pthread_mutex_unlock(&philos->params->m_print);
 	pthread_mutex_destroy(&philos->params->m_print);
+	pthread_mutex_unlock(&philos->params->m_stop);
 	pthread_mutex_destroy(&philos->params->m_stop);
 	free (philos->params->m_forks);
 	free (philos->params);
 	free (philos);
+	return (1);
 }
 
 static int	launch_philo(t_philo *philos, int cmp)
@@ -36,10 +41,7 @@ static int	launch_philo(t_philo *philos, int cmp)
 static int	launch_all(t_philo *philos)
 {
 	philos->params->t_started = get_time();
-	if (launch_philo(philos, 0))
-		return (1);
-	ft_usleep(100);
-	if (launch_philo(philos, 1))
+	if (launch_philo(philos, 0) || launch_philo(philos, 1))
 		return (1);
 	return (0);
 }
@@ -51,7 +53,7 @@ int	main(int argc, char *argv[])
 	if (init_all(&philos, argc, argv))
 		return (1);
 	if (launch_all(philos))
-		return (1);
+		return (free_all(philos));
 	pthread_mutex_lock(&philos->params->m_stop);
 	free_all(philos);
 	return (0);
